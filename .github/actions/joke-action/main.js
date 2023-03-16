17cb1d0056c4c946f45c9fca683a2011d3e27d54
Ayoub <94688852+NuwGen@users.noreply.github.com>
2023-03-16 16:04:41 +0100

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,7 +1,7 @@
 const getJoke = require("./joke");
 const core = require("@actions/core");
 
-async funciton run() {
+async function run() {
  const joke = await getJoke();
  console.log(joke);
  core.setOutput("joke-output", joke);
